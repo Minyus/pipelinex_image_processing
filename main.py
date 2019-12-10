4df7b922abd5517a8513e6e160c5882148b4fcd1
Yusuke Minami <33908456+Minyus@users.noreply.github.com>
2019-12-10 18:34:20 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from pathlib import Path
+
+project_path = Path(__file__).resolve().parent
+
+src_path = project_path / "src"
+if str(src_path) not in sys.path:
+    sys.path.insert(0, str(src_path))
+
+if "PYTHONPATH" not in os.environ:
+    os.environ["PYTHONPATH"] = src_path
+
+import pipelinex
+print("pipelinex version: ", pipelinex.__version__)
+
+from pipelinex import MLflowFlexibleContext
+
+
+context = MLflowFlexibleContext(project_path)
+context.run()

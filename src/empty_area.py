4c767a530c8ed4d18be83cad1a76a0030176d531
Minyus <yusuke.minami86@gmail.com>
2019-12-11 10:23:28 +0800

    Extract and overlay depth line segments

--- src/empty_area.py
+++ src/empty_area.py
@@ -13,17 +13,43 @@ def overlay_line_segments(img):
     min_length = 0.02 * w
     zeros = np.zeros_like(img)
     lines_img = np.zeros_like(img)
+    line_points_list = []
     for line in lines:
         pt1, pt2 = list_to_arrays(line, c)
-        pt1, pt2 = extend(pt1, pt2, scale=-3)
+        pt1, pt3 = extend(pt1, pt2, scale=-3)
         if min_length < norm(pt1 - pt2) < 0.8 * norm(pt1 - pt2, ord=1):
             line_img = cv2.line(
-                zeros, pt1=tuple(pt1), pt2=tuple(pt2), color=1, thickness=5
+                zeros, pt1=tuple(pt1), pt2=tuple(pt3), color=1, thickness=5
             )
             lines_img += line_img
+            line_points_list.append((pt1, pt2))
+    lines_img = cv2.GaussianBlur(lines_img, ksize=(51, 51), sigmaX=51)
+    pt0_yx = argsmax(lines_img)
+    pt0 = pt0_yx[::-1]
+    max_val = int(np.max(lines_img))
+    lines_img = cv2.line(
+        lines_img, pt1=tuple(pt0), pt2=tuple(pt0), color=max_val * 2, thickness=10
+    )
     lines_img = (255 * (lines_img / np.max(lines_img, keepdims=True))).astype(np.uint8)
-    cross_pt = argsmax(lines_img)
-    return lines_img
+
+    depth_line_points_list = []
+    for line_points in line_points_list:
+        pt1, pt2 = line_points
+        v12 = pt2 - pt1
+        nv12 = v12 / norm(v12)
+        v01 = pt1 - pt0
+        cross_dist = np.abs(nv12[0] * v01[1] - nv12[1] * v01[0])
+        depth_flag = cross_dist < 10
+        if depth_flag:
+            depth_line_points_list.append(line_points)
+
+    depth_line_img = np.zeros_like(img)
+    for depth_line_points in depth_line_points_list:
+        pt1, pt2 = depth_line_points
+        depth_line_img = cv2.line(
+            depth_line_img, pt1=tuple(pt1), pt2=tuple(pt2), color=255, thickness=1
+        )
+    return depth_line_img
 
 
 def list_to_arrays(line, c):
